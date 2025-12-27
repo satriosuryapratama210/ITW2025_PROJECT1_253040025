48ae47fbf545ed7add5ad601722a9d4be8e8bb6d
satriosuryapratama222@gmail.com <satriosuryapratama222@gmail.com>
2025-12-28 00:21:57 +0700

    menyelesaikan tugas besar vanilla

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click',function() {
-    nav.classList.toggle('slide');});
+    nav.classList.toggle('slide');}); 
